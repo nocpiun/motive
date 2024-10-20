2094143b426498f0df3332e239e8c9c290427079
NriotHrreion <nriot233@outlook.com>
2024-10-20 10:26:33 +0800

    feat: Add webpage info in console

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,13 @@ import { Motive } from "./simulator";
 console.log(
     "\n%cMotive%cv"+ version +" | Nocpiun Org\n"+
     "%cA web-based physics simulator.\n\n"+
-    "%cWebpage: %cComing soon...\n"+
+    "%cWebpage: https://motive.nin.red\n"+
     "%cGithub Repo: https://github.com/nocpiun/motive\n\n"+
     "%cDonate: https://nin.red/#/donate\n",
     "font-family: Consolas;font-size: 17pt;font-weight: bold;padding: 10px",
     "font-family: Consolas;font-size: 8pt;color: gray",
     "font-family: Consolas;font-size: 8pt;color: white",
     "font-family: Consolas;font-size: 8pt;color: white",
-    "font-family: Consolas;font-size: 8pt;color: gray;font-style: italic",
     "font-family: Consolas;font-size: 8pt;color: white",
     "font-family: Consolas;font-size: 8pt;color: white"
 );
