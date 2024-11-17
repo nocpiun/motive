38308fca442b9f4a56e1b8d3dde5989231d4b85c
NriotHrreion <nriot233@outlook.com>
2024-11-17 09:12:45 +0800

    i18n: Add zh-tw & perf: Modify font style

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,9 +1,11 @@
 import zh_cn from "./zh-cn.json";
+import zh_tw from "./zh-tw.json";
 import en_us from "./en-us.json";
 
 export function getLocale(lang: string): { [key: string]: string } {
     switch(lang) {
         case "zh-cn": return zh_cn;
+        case "zh-tw": return zh_tw;
         case "en-us": return en_us;
     }
 }
